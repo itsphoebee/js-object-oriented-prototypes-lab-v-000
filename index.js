8351929ac7ab594fdd28849b003377f5c9480046
Phoebe Lau <phoebelau7@gmail.com>
2018-03-27 05:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@
 BoardMember.prototype.veto = function(){
      return 'No, I must disagree'
    }
-  
+
 BoardMember.prototype.approve = function(){
      return 'You can do that!'
    }
-   
+
 BoardMember.prototype.doCharity = function(){
      return "I like to help people."
    }
